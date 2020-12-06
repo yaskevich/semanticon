8ffda746de602c3fbcb40f2062afc33bcc6e3127
yaskevich <alyaxey.yaskevich@gmail.com>
2020-12-06 23:53:32 +0300

    receive command line arguments

--- parser.js
+++ parser.js
@@ -80,7 +80,7 @@ const phrasesInsert = `INSERT INTO phrases
                     VALUES($1::jsonb, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
                     RETURNING *`;
 //
-const pgIds = {};
+const tokenIds = {};
 const featureIds = {};
 //
 
@@ -116,142 +116,156 @@ async function checkFeatureArray(fld, content) {
     return JSON.stringify(thisArrIds);
 }
 
+
+async function vectorizeTokens(content){
+	// console.time();
+	// those chains of loops are to force code to run PG queries sequentially
+	const unitsArr = content.split("|");
+	const unitsArrVector = [];
+	for (let i2=0; i2 < unitsArr.length; i2++) {
+		const tokensArr = unitsArr[i2].split(/\s|(?=-)/g);
+		const tokensArrVector = [];
+		for (let t=0; t < tokensArr.length; t++) {
+			const tkn = tokensArr[t];
+			let id  = 0;
+			if (Reflect.getOwnPropertyDescriptor(tokenIds, tkn)) {
+				id  = tokenIds[tkn];
+			} else {
+				try {
+					const result  = await pool.query(tokensInsert, [tkn]);
+					tokenIds[tkn] = id = result.rows[0].id;
+				} catch (e){
+					console.error(e.detail);
+				}
+			}
+			tokensArrVector.push(id);
+		}
+		unitsArrVector.push(tokensArrVector);
+	}
+	// console.timeEnd();
+	return JSON.stringify(unitsArrVector);
+}
+
 async function processFile(fileName) {
 
-    if(!fs.existsSync(fileName)){
-        process.exit();
-    }
+    if(fs.existsSync(fileName)){
+		const csvString = fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
+		let csvArr  = [];
 
-    const csvString = fs.readFileSync(path.join(__dirname, fileName), 'utf-8');
+		try {
+			csvArr = await csv.parse(csvString, {delimiter: ";"});
+		}
+		catch(e) {
+			console.log(e.message);
+		}
 
-    let csvArr  = [];
+		const fieldRow = csvArr.shift();
+		const dict = fieldRow.map(x => mappingRuEn[x]);
 
-    try {
-        csvArr = await csv.parse(csvString, {delimiter: ";"});
-    }
-    catch(e) {
-        console.log(e.message);
-    }
+		const dump = {};
+		const mappingEnRu = Object.assign({}, ...Object.entries(mappingRuEn).map(([a,b]) => ({ [b]: a })));
 
-    const fieldRow = csvArr.shift();
-    const dict = fieldRow.map(x => mappingRuEn[x]);
+		await pool.query('DROP table IF EXISTS features');
+		await pool.query('DROP table IF EXISTS phrases');
+		await pool.query('DROP table IF EXISTS tokens');
 
-    const dump = {};
-    const mappingEnRu = Object.assign({}, ...Object.entries(mappingRuEn).map(([a,b]) => ({ [b]: a })));
+		await pool.query(phrasesSheme);
+		await pool.query(tokensSheme);
+		await pool.query(featuresSheme);
 
-    await pool.query('DROP table IF EXISTS features');
-    await pool.query('DROP table IF EXISTS phrases');
-    await pool.query('DROP table IF EXISTS tokens');
+		for (const row of csvArr) {
+			const values = [];
+			let semantics1 = "";
+			for (let i=0; i < row.length; i++) {
+				const data  = row[i];
+				const fieldEn = dict[i];
+				// const fieldRu = fieldRow[i];
+				if(fieldEn  === "unit") {
+					const result = await vectorizeTokens(data);
+					values.push(result);
+				} else if(fieldEn  === "extrequired") {
+					values.push(data?1:0);
+				} else if(fieldEn  === "semantics1") {
+					semantics1 = data;
+				} else if(fieldEn === "semantics") {
+					const result = await checkFeatureArray(fieldEn, semantics1 + "|" + data);
+					values.push(result);
+				} else if(["act1", "extension", "gest", "organ"].includes(fieldEn)) {
+					const result = await checkFeatureArray(fieldEn, data);
+					values.push(result);
+				} else if(fieldEn === "actclass") {
+					// empty = error!!!
+					if(!data) {
+						console.log(fieldEn, "is empty!");
+					}
+					const result = await checkFeatureArray(fieldEn, data);
+					values.push(result);
+				} else if(fieldEn === "situation") {
+					values.push(data);
+				} else if(fieldEn === "parts") {
+					// !!! empty I treat as two-parts !!!
+					// parts boolean not null default false,
+					values.push(data==="трехчастная"?1:0);
+				} else if(fieldEn === "intonation") {
+					const result = await checkFeature(fieldEn, data);
+					values.push(result);
+				} else if(fieldEn === "mods") {
+					values.push(data);
+				} else {
+					// console.log(data);
+				}
+				
+				
+				// aggregate data for debugging
+				if (Reflect.getOwnPropertyDescriptor(dump, fieldEn)) {
+					const place  = dump[fieldEn]["values"].indexOf(data);
+					if (place === -1){
+						dump[fieldEn]["values"].push(data);
+						dump[fieldEn]["counts"].push(1);
+					} else {
+						dump[fieldEn]["counts"][place] +=1;
+					}
+				} else {
+					dump[fieldEn] = { "counts" : [1], "values": [data] };               
+				}
+				// aggregate data for debugging
+			}
+		
+			try {
+			  await pool.query(phrasesInsert, values);
+			} catch (err) {
+			  console.log(err.stack);
+			}
+		}
+		
+		await pool.end();
 
-    await pool.query(phrasesSheme);
-    await pool.query(tokensSheme);
-    await pool.query(featuresSheme);
+		let out = "";
+		dict.forEach(function(item) {
+			out+="=============================\n";
+			out+="=============================\n";
+			out+= item +  "||" + mappingEnRu[item] + "\n";
+			out+="=============================\n";
+			dump[item]["counts"].forEach(function(a, b) {
+				const unit = dump[item]["values"][b] || '■';
+				out+= `${unit}\t${a}\n`;
+			});
+			
+		});
 
-    for (const row of csvArr) {
-        let values = [];
-        let semantics1 = "";
-        for (let i=0; i < row.length; i++) {
-            const data  = row[i];
-            const fieldEn = dict[i];
-            // const fieldRu = fieldRow[i];
-            if(fieldEn  === "unit") {
-                // console.time();
-                // those chains of loops are to force code to run PG queries sequentially
-                const unitsArr = data.split("|");
-                const unitsArrVector = [];
-                for (let i2=0; i2 < unitsArr.length; i2++) {
-                    const tokensArr = unitsArr[i2].split(/\s|(?=-)/g);
-                    const tokensArrVector = [];
-                    for (let t=0; t < tokensArr.length; t++) {
-                        const tkn = tokensArr[t];
-                        let id  = 0;
-                        if (Reflect.getOwnPropertyDescriptor(pgIds, tkn)) {
-                            id  = pgIds[tkn];
-                        } else {
-                            try {
-                                const result  = await pool.query(tokensInsert, [tkn]);
-                                pgIds[tkn] = id = result.rows[0].id;
-                            } catch (e){
-                                console.error(e.detail);
-                            }
-                        }
-                        tokensArrVector.push(id);
-                    }
-                    unitsArrVector.push(tokensArrVector);
-                }
-                // console.timeEnd();
-                values.push(JSON.stringify(unitsArrVector));
-            } else if(fieldEn  === "extrequired") {
-                values.push(data?1:0);
-            } else if(fieldEn  === "semantics1") {
-                semantics1 = data;
-            } else if(fieldEn === "semantics") {
-                const result = await checkFeatureArray(fieldEn, semantics1 + "|" + data);
-                values.push(result);
-            } else if(["act1", "extension", "gest", "organ"].includes(fieldEn)) {
-                const result = await checkFeatureArray(fieldEn, data);
-                values.push(result);
-            } else if(fieldEn === "actclass") {
-                // empty = error!!!
-                if(!data) {
-                    console.log(fieldEn, "is empty!");
-                }
-                const result = await checkFeatureArray(fieldEn, data);
-                values.push(result);
-            } else if(fieldEn === "situation") {
-                values.push(data);
-            } else if(fieldEn === "parts") {
-                // !!! empty I treat as two-parts !!!
-                // parts boolean not null default false,
-                values.push(data==="трехчастная"?1:0);
-            } else if(fieldEn === "intonation") {
-                const result = await checkFeature(fieldEn, data);
-                values.push(result);
-            } else if(fieldEn === "mods") {
-                values.push(data);
-            } else {
-                // console.log(data);
-            }
-            
-            
-            // aggregate data for debugging
-            if (Reflect.getOwnPropertyDescriptor(dump, fieldEn)) {
-                const place  = dump[fieldEn]["values"].indexOf(data);
-                if (place === -1){
-                    dump[fieldEn]["values"].push(data);
-                    dump[fieldEn]["counts"].push(1);
-                } else {
-                    dump[fieldEn]["counts"][place] +=1;
-                }
-            } else {
-                dump[fieldEn] = { "counts" : [1], "values": [data] };               
-            }
-            // aggregate data for debugging
-        }
-    
-        try {
-          await pool.query(phrasesInsert, values);
-        } catch (err) {
-          console.log(err.stack);
-        }
-    }
-    
-    await pool.end();
+		fs.writeFileSync( "agg.log", out, "utf8");        
+		
+    } else {
+		console.log("Path to the file with data is incorrect!");
+	}
 
-    let out = "";
-    dict.forEach(function(item) {
-        out+="=============================\n";
-        out+="=============================\n";
-        out+= item +  "||" + mappingEnRu[item] + "\n";
-        out+="=============================\n";
-        dump[item]["counts"].forEach(function(a, b) {
-            const unit = dump[item]["values"][b] || '■';
-            out+= `${unit}\t${a}\n`;
-        });
-        
-    });
-
-    fs.writeFileSync( "agg.log", out, "utf8");
 }
 // entry point
-(async () => { await processFile(process.env.CSV); })();
+(async () => { 
+	if (process.argv[2]) {
+		await processFile(process.argv[2]); 
+	} else {
+		console.log("Put path to the file containing data as a command-line argument!");
+	}
+	
+})();
