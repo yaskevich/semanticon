952f09ccdb4392805e40f58578b5f1cdb5efd7fa
yaskevich <alyaxey.yaskevich@gmail.com>
2020-12-08 23:41:19 +0300

    queries for API

--- index.js
+++ index.js
@@ -4,10 +4,43 @@ const { Pool, Client } = require('pg')
 // pools will use environment variables
 // for connection information
 const pool = new Pool()
-pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name", (err, res) => {
-  console.log(err, res)
-  pool.end()
+pool.query("select units.id, units.pid from units left join phrases on units.pid=phrases.pid limit 5", (err, res) => {
+	if (!err){
+		const data  = res.rows;
+		console.log(JSON.stringify(data));
+		
+		pool.query("select * from tokens", (err, res) => {
+			if (!err){
+				const dict = res.rows.reduce((obj, item) => (obj[item.id.toString()] = item.token, obj) ,{});
+				// console.log(dict);
+				pool.query("select pid, phrase->0 as phrase from phrases", (err, res) => {
+				if (!err){
+						// console.log(res.rows);
+						const index = res.rows.reduce((obj, item) => (obj[item.pid.toString()] = item.phrase, obj) ,{});
+						console.log(index);
+						for(let i=0; i<data.length; i++){
+							const entry = data[i];
+							// console.log(entry["id"]);
+							const phrase = index[entry["pid"]];
+							// for (let x=0; x<phrase.length; x++){
+								// console.log("!", phrase[x]);
+								
+							// }
+							
+							const kek  = phrase.map(x => dict[x]).join(" ").replace(/ (?=-)/, '');
+							console.log(entry["pid"], kek);
+						}
+					}
+				  pool.end()
+				})
+			}
+		  // pool.end()
+		})
+	}
+  // pool.end()
 })
+
+// select * from tokens where id in (4,1,6,2,3);
 // you can also use async/await
 // const res = await pool.query('SELECT NOW()')
 // await pool.end()
