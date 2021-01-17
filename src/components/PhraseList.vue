c688176f6ed8a89ec16c38a38327574f942b0fd0
yaskevich <alyaxey.yaskevich@gmail.com>
2021-01-17 17:35:15 +0300

    create feature object with all the features (for filters)

--- src/components/PhraseList.vue
+++ src/components/PhraseList.vue
@@ -4,27 +4,36 @@
 
   <div class="p-grid p-m-2">
     <div class="p-col p-text-center">
-      <Dropdown v-model="semfunc" :options="semfuncArray" optionLabel="name" placeholder="Основная функция" scrollHeight="300" :showClear="true" class="semfunc" @change="updateRoute($event, 'semfunc')"/>
+      <Dropdown v-model="semfunc" :options="aggregatedFeatures['semfunc']" optionLabel="name" placeholder="Основная функция" scrollHeight="300" :showClear="true" class="semfunc" @change="updateRoute($event)"/>
     </div>
   </div>
 
   <div class="p-d-flex p-flex-column p-col">
-    <MultiSelect v-model="semtone"  filterPlaceholder="Наберите название" :filter="true"  :options="semtoneResult" optionLabel="name" placeholder="Дополнительная семантика" display="chip" class="" @change="updateRoute($event, 'semtone')" />
+    <MultiSelect v-model="semtone"  filterPlaceholder="Наберите название" :filter="true"  :options="aggregatedFeatures['semtone']" optionLabel="name" placeholder="Дополнительная семантика" display="chip" class="" @change="updateRoute($event)" />
   </div>
 
   <div class="p-d-flex p-flex-column p-col">
-    <MultiSelect v-model="actclass"  filterPlaceholder="Наберите название" :filter="true" :options="actclassResult" optionLabel="name" placeholder="Реплика-стимул" display="comma" @change="updateRoute($event, 'actclass')" />
+    <MultiSelect v-model="actclass"  filterPlaceholder="Наберите название" :filter="true" :options="aggregatedFeatures['actclass']" optionLabel="name" placeholder="Реплика-стимул" display="comma" @change="updateRoute($event)" />
   </div>
 
   <div class="p-fluid p-col" >
     <div class="p-field p-grid" >
       <label for="partsbutton" class="p-col-12 p-mb-2 p-md-2 p-mb-md-0 p-component">Структура</label>
       <div class="p-col-12 p-md-10">
-        <SelectButton v-model="parts" :options="partsOptions"  class="" optionLabel="name" optionValue="code" id="partsbutton" @click="updateRoute($event, 'parts')" />
+        <SelectButton v-model="parts" :options="partsOptions"  class="" optionLabel="name" optionValue="code" id="partsbutton" @click="updateRoute($event)" />
       </div>
     </div>
   </div>
-
+  <Panel header="Ещё фильтры" :toggleable="true" :collapsed="true">
+    <div class="p-grid">
+    <div class="p-col p-text-center">1</div>
+    <div class="p-col p-text-center">2</div>
+    <div class="p-col p-text-center">3</div>
+    </div>
+    <!-- <Dropdown v-model="semfunc" :options="semfuncOptions" optionLabel="name" placeholder="Жесты" scrollHeight="300" :showClear="true" class="semfunc p-mr-4" @change="updateRoute($event, 'semfunc')"/>
+    <Dropdown v-model="semfunc" :options="semfuncOptions" optionLabel="name" placeholder="Интонация" scrollHeight="300" :showClear="true" class="semfunc" @change="updateRoute($event, 'semfunc')"/>
+    <Dropdown v-model="semfunc" :options="semfuncOptions" optionLabel="name" placeholder="Язык" scrollHeight="300" :showClear="true" class="semfunc" @change="updateRoute($event, 'semfunc')"/> -->
+  </Panel>
 
 
   <PhraseListItem v-for="eid in eids" :key="eid" :data="data" :eid="Number(eid)" />
@@ -51,26 +60,22 @@ export default {
 		let semtone = ref(null);
 		let actclass = ref(null);
     let semfunc = ref(null);
+    let eids = ref([]);
+    eids.value = Object.keys(store.state.config.toc);
+
     if (routerInfo.params.id) {
       // semtone.value = [{"value": routerInfo.params.id, "name": data.features[routerInfo.params.id][0]}];
       semfunc.value = {"value": routerInfo.params.id, "name": data.features[routerInfo.params.id][0]};
     }
-    let semtoneResult = ref([]);
-    let actclassResult = ref([]);
-    let eids = ref([]);
-    eids.value = Object.keys(store.state.config.toc);
 
-    const semfuncArray = Object.keys(data.features).filter(x => data.features[x][1]==="semfunc").map(x => ({"value": x, "name": data.features[x][0]}));
+    const aggregatedFeatures = Object.keys(data.features)
+    .map((key) => ({ "value": Number(key), "name": data.features[key][0], "feature": data.features[key][1]}))
+    .reduce((obj, x) => ({ ...obj, [x["feature"]]: [...(obj[x["feature"]] || []), x, ],}),{},);
 
-
-    actclassResult.value = Object.keys(data.features).filter(x => data.features[x][1]==="actclass").map(x => ({"value": x, "name": data.features[x][0]}));
-
-    semtoneResult.value  = Object.keys(data.features).filter(x => data.features[x][1]==="semtone").map(x => ({"value": x, "name": data.features[x][0]}));
-
-    const updateRoute = (e, title) => {
+    const updateRoute = (e) => {
       // let curTarget = e.currentTarget;
       // let curTargetData = curTarget.dataset;
-      console.log("update on change", `|${title}|`, e.value);
+      console.log("update on change", e.value);
       // router.push("/home");
       // const actclassValues  =
       const semtoneValues = semtone.value ? semtone.value.map(y=>Number(y.value)) : null;
@@ -115,42 +120,11 @@ export default {
       eids.value = selected;
     };
 
-    const rebuildList = () => {
-      const selected = [];
-      if (semtone.value) {
-          const vals = semtone.value.map(y=>Number(y.value));
-          for (let [k, v] of Object.entries(store.state.config.toc)) {
-            let sems = Object.values(v).flatMap(x=>x).flatMap(x=> data.units[x].semtone);
-            if (vals.every(y => sems.includes(y))) {
-              selected.push(k);
-            }
-          }
-        }
-      eids.value = selected;
-    };
-
     if (routerInfo.params.id){
-      rebuildList();
+      console.log("router", routerInfo.params.id);
     }
-    // watchEffect(() => {
-    //   // if (){
-    //     console.log("watch");
-    //     if(semtone.value !== null && !routerInfo.params.id){
-    //       setTimeout((x) => {
-    //         if ((!x && !semtone.value) || (x && semtone.value && (semtone.value.sort().toString() == x.sort().toString()))) {
-    //             console.log("update", semtone.value, x);
-    //             rebuildList();
-    //         } else {
-    //           console.log("wait");
-    //         }
-    //       }, 1000, semtone.value);
-    //     } else {
-    //       console.log("is null");
-    //     }
-    //   // }
-    // });
-  return { data, eids, semtone, semtoneResult, updateRoute, semfunc,
-semfuncArray, parts, partsOptions, actclass, actclassResult };
+
+  return { data, eids, semtone, semfunc, actclass, parts, partsOptions, aggregatedFeatures, updateRoute };
 },
   components: {
     // eslint-disable-next-line vue/no-unused-components
