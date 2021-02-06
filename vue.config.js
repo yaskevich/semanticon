8fbb03c7d829271ad43bf04efd8602fec663c931
yaskevich <alyaxey.yaskevich@gmail.com>
2021-02-07 00:04:33 +0300

    change server port for debug

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
 		open: false,
 		proxy: {
 			"/api": {
-				target: "http://localhost:5000",
+				target: "http://localhost:8000",
 				changeOrigin: true,
 				ws:true,
 				// pathRewrite: {
