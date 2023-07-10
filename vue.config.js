f8570a5b6aabe4c2a5801d27b11dd4bb1391a4c1
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-07-10 15:40:24 +0200

    update configs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 //vue.config.js
-const webpack = require('webpack');
+// const webpack = require('webpack');
 module.exports = {
   pages: {
     index: {
@@ -8,43 +8,37 @@ module.exports = {
       template: 'public/index.html',
       // output as dist/index.html
       filename: 'index.html',
-      title: 'Прагматикон'
-    }
+      title: 'Прагматикон',
+    },
   },
   configureWebpack: {
     performance: {
-      hints: "warning",
+      hints: 'warning',
       maxEntrypointSize: 1024000,
-      maxAssetSize: 512000
+      maxAssetSize: 512000,
     },
-    plugins: [
-      // Define Bundler Build Feature Flags
-      new webpack.DefinePlugin({
-        // Drop Options API from bundle
-        "__VUE_OPTIONS_API__": false,
-        "__VUE_PROD_DEVTOOLS__": false,
-      }),
-    ]},
+    plugins: [],
+  },
   devServer: {
     port: 3010,
-    host: "localhost",
+    host: 'localhost',
     https: false,
     open: false,
     proxy: {
-      "/api": {
-        target: "http://localhost:3011",
+      '/api': {
+        target: 'http://localhost:3011',
         changeOrigin: true,
-        ws:true,
+        ws: true,
         // pathRewrite: {
-          // "^/api": ""
+        // "^/api": ""
         // },
-        logLevel: 'debug'
+        logLevel: 'debug',
       },
       // "/media": {
       //   target: "http://localhost:5000",
       //   changeOrigin: true,
       //   logLevel: 'debug'
       // }
-    }
-  }
-}
+    },
+  },
+};
