a819f66f57a2e15651a172adce86c4e0d77d6b61
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-07-13 15:11:33 +0200

    load package info and data statically in store

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,104 +1,114 @@
-import { reactive } from "vue";
-import axios from "axios";
-import router from "../router";
+import { reactive } from 'vue';
+// import axios from 'axios';
+// import router from '../router';
+import project from '../../package.json';
+
+const media = process.env.VUE_APP_MEDIA
+  ? location.protocol + '//' + process.env.VUE_APP_MEDIA
+  : document.location.origin + '/api/media';
 
 const state = reactive({
   accessed: [],
   about: {
-    active: 3
+    active: 3,
   },
   autocomplete: {
-    "checked": false,
-    "mode": "ru",
-    "text": '',
-    "matches": {},
+    checked: false,
+    mode: 'ru',
+    text: '',
+    matches: {},
   },
-  search:  {
-   'semtone' : [],
-   'actclass' : [],
-   'organ' : [],
-   'semfunc' : null,
-   'intonation' : null,
-   'translations' : null,
-   'parts' : null, // was false => null or change UI to radiobuttons
- },
+  search: {
+    semtone: [],
+    actclass: [],
+    organ: [],
+    semfunc: null,
+    intonation: null,
+    translations: null,
+    parts: null, // was false => null or change UI to radiobuttons
+  },
   config: {
-    "user": {},
-    "test": 1,
-    "features": {},
-    "pages": null,
-  }
+    user: {},
+    test: 1,
+    features: {},
+    pages: null,
+  },
 });
 
-const getUser = () => {
-  if (!state.actions.isAuth()) {
-    axios.get("/api/user")
-        .then((response) => {
-            state.actions.set("user", response.data.user);
-        })
-        .catch((errors) => {
-            console.log(errors)
-            // router.push("/")
-        })
-  }
-};
+if (process.env.VUE_APP_DATA) {
+  state.config = await import(process.env.VUE_APP_DATA);
+}
 
-const doLogout = () => {
-  axios.get("/api/logout")
-      // .then((response) => {
-      .then(() => {
-        state.actions.set("user", {});
-        // console.log(response);
-        // router.push("/")
-        // router.go('/');
-      })
-      .catch((errors) => {
-          console.log(errors)
-      })
-};
+// const getUser = () => {
+//   if (!state.actions.isAuth()) {
+//     axios
+//       .get('/api/user')
+//       .then(response => {
+//         state.actions.set('user', response.data.user);
+//       })
+//       .catch(errors => {
+//         console.log(errors);
+//       });
+//   }
+// };
 
-const doLogin = (email, password) => {
-  if (!state.actions.isAuth()) {
-    let payload = {
-            email: email,
-            password: password
-        }
-        axios.post("/api/login", payload)
-            .then((response) => {
-                state.actions.set("user", response.data.user);
-                router.push("/dashboard")
-            })
-            .catch((errors) => {
-                console.log("Cannot log in", errors)
-            })
-    }
-};
+// const doLogout = () => {
+//   axios
+//     .get('/api/logout')
+//     // .then((response) => {
+//     .then(() => {
+//       state.actions.set('user', {});
+//       // console.log(response);
+//       // router.push("/")
+//       // router.go('/');
+//     })
+//     .catch(errors => {
+//       console.log(errors);
+//     });
+// };
+
+// const doLogin = (email, password) => {
+//   if (!state.actions.isAuth()) {
+//     let payload = {
+//       email: email,
+//       password: password,
+//     };
+//     axios
+//       .post('/api/login', payload)
+//       .then(response => {
+//         state.actions.set('user', response.data.user);
+//         router.push('/dashboard');
+//       })
+//       .catch(errors => {
+//         console.log('Cannot log in', errors);
+//       });
+//   }
+// };
 
 const getData = async () => {
-    try {
-      const endpoint  = "/api/data";
-      const getApiData = await fetch(endpoint);
-      const datum = await getApiData.json();
-      // console.log(`API [${endpoint}] →  app`);
-      state.config = datum;
-      // if (Object.keys(datum.user).length){
-      //   state.actions.set("user", datum.user);
-      // }
-    } catch (e) {
-      // state.errors[key] = e;
-      console.log(e);
-    }
+  try {
+    const endpoint = '/api/data';
+    const getApiData = await fetch(endpoint);
+    const datum = await getApiData.json();
+    // console.log(`API [${endpoint}] →  app`);
+    state.config = datum;
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 export default {
   // state: readonly(state),
-  backend: {
-    getUser,
-    doLogin,
-    doLogout,
-    getData
-  },
-  state: state,
+  // backend: {
+  // getUser,
+  // doLogin,
+  // doLogout,
+  // getData,
+  // },
+  getData,
+  version: project.version,
+  state,
+  media,
   actions: {
     isAuth() {
       return Boolean(Object.keys(state.config.user).length);
@@ -106,5 +116,5 @@ export default {
     set(key, value) {
       state.config[key] = value;
     },
-  }
+  },
 };
