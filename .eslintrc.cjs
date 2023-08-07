59eb5022d15acc359233b70acaf3646c074bb211
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-08-07 12:00:59 +0200

    update ESLint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'import/extensions': ['error', 'ignorePackages'],
+    'import/no-extraneous-dependencies': 'off',
+    'comma-dangle': 'off',
+    'no-plusplus': 'off',
+    'max-len': 'off',
+    'no-await-in-loop': 'off',
+    'no-restricted-syntax': 'off'
+  },
+};
