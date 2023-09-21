5957c389745672d868f579bc7e5031e9bee1ecb0
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-09-21 13:02:52 +0200

    move stats to db

--- db.js
+++ db.js
@@ -579,6 +579,79 @@ async function processCSV(csvContent, schemeName) {
 }
 
 /// ///////////////////////////////////////////////////////////////////////////////////////////
+const getStats = async () => {
+  const resultCountTokens = await pool.query('SELECT COUNT(*) FROM tokens');
+  const resultCountPhrases = await pool.query('SELECT COUNT(*) FROM phrases');
+  const resultCountExprs = await pool.query('SELECT COUNT(*) FROM exprs');
+
+  const resultTokens = await pool.query('SELECT * FROM tokens');
+  const resultPhrases = await pool.query('SELECT * FROM phrases');
+  const resultExprs = await pool.query('SELECT * FROM exprs');
+
+  const countTokens = resultCountTokens.rows[0].count;
+  const countPhrases = resultCountPhrases.rows[0].count;
+  const countExprs = resultCountExprs.rows[0].count;
+  console.log('tokens / phrases / exprs', countTokens, countPhrases, countExprs);
+
+  const tokensObject = Object.assign({}, ...(resultTokens.rows.map((x) => ({ [x.id]: x.token }))));
+
+  const exprsObject = Object.assign({}, ...(resultExprs.rows.map((x) => ({ [x.eid]: x.expr }))));
+  // console.log("",  tokensObject);
+
+  // console.log(resultPhrases.rows);
+  const lenObj = {};
+  for (const x of resultPhrases.rows) {
+    const len = String(x.phrase.length);
+    if (lenObj?.[len]) {
+      lenObj[len] += 1;
+    } else {
+      lenObj[len] = 1;
+    }
+    // if (len > 15) {
+    // console.log("LEN", len, x.phrase);
+    // for (let eid of x.phrase) {
+    // console.log(exprsObject[eid].map(x => tokensObject[x]));
+    // }
+    // }
+  }
+  // console.log(lenObj);
+
+  const occObj = {};
+  // console.log(exprsObject);
+  // eslint-disable-next-line guard-for-in
+  for (const x in exprsObject) {
+    // console.log(exprsObject[x]);
+    for (const tid of exprsObject[x]) {
+      if (occObj?.[tid]) {
+        occObj[tid] += 1;
+      } else {
+        occObj[tid] = 1;
+      }
+    }
+  }
+
+  const countOcc = Object.values(occObj).reduce((a, b) => a + b, 0);
+  console.log('count occ', countOcc);
+
+  const sortedOcc = Object.entries(occObj).sort((a, b) => b[1] - a[1]);
+  // const globalPercent = 0;
+
+  let top = 0;
+
+  for (const x of sortedOcc) {
+    const per = Math.round((x[1] / countOcc + Number.EPSILON) * 100);
+    const qty = x[1];
+    //   globalPercent += per;
+    if (per > 0) {
+      top += qty;
+      console.log(`${tokensObject[x[0]].padStart(15)} ${String(qty).padStart(10)} \t ${per}%`);
+      // console.log(tokensObject[x[0]]);
+      // console.log(qty);
+    }
+  }
+
+  console.log('top', top, Math.round((top / countOcc + Number.EPSILON) * 10000));
+};
 
 const getTranslations = async () => formatSQL('SELECT * FROM translations', 'id', (x) => (x));
 
@@ -677,4 +750,5 @@ export default {
   getTokens,
   getData,
   processCSV,
+  getStats,
 };
