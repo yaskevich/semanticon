fd799c2bcdca956d0e1acb3679f939a7968f8707
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-10-08 22:56:07 +0200

    refactor

--- src/components/Example.vue
+++ src/components/Example.vue
@@ -6,30 +6,21 @@
       <span v-else title="публикация" class="ml-1 valign"><i class="pi pi-book"></i></span>
       <span class="example-author ml-1">{{ datum.author }}.</span>
       <span class="example-pub ml-1 font-bold">{{ datum.pub }}</span>
-      <span v-if="datum.journal" class="ml-1 example-journal" title="публикация в журнале">(«{{ v.journal }}»)</span>
+      <span v-if="datum.journal" class="ml-1 example-journal" title="публикация в журнале">
+        (&laquo;{{ v.journal }}&raquo;)
+      </span>
       <span class="example-pubdate">, {{ datum.pubdate }}</span>
     </template>
   </div>
 </template>
 
-<script>
+<script setup>
 import store from '../store';
-
-export default {
-  name: 'Example',
-  props: {
-    datum: Object,
-  },
-  setup() {
-    return { store }
-  },
-  components: {},
-};
+const props = defineProps(['datum'])
 </script>
 
 <style scoped>
 .example {
-  /* border: 1px solid orange; */
   padding: 0.5rem;
 }
 
@@ -39,19 +30,19 @@ export default {
 .example-text {
   /* font-stretch: condensed; */
   letter-spacing: -1px;
-  font-style: italic;
+  /* font-style: italic; */
 }
 
-.example-author {
-  /* color: purple; */
-}
+/* .example-author {
+  color: purple;
+} */
 
 .example-pub {
   color: #f5cb5c;
   text-shadow: black 0 0 0.5px;
 }
 
-.example-pubdate {
-  /* color: blue; */
-}
+/* .example-pubdate {
+  color: blue;
+} */
 </style>
