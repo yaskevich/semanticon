a7a6343311140c09d125f72cbae1632c60960d78
Alyaxey Yaskevich <alyaxey.yaskevich@gmail.com>
2023-10-09 20:45:29 +0200

    refactor

--- src/components/UnitList.vue
+++ src/components/UnitList.vue
@@ -2,12 +2,12 @@
   <div class="p-component p-3">
     <div v-for="value in data.toc[$route.params.id]" :key="value">
       <span v-if="data.units[value[0]]['phrase'].length != 1">
-        <!-- Варианты: -->
+        <!-- Variants: -->
         <span v-for="(variant, index) in data.units[value[0]]['phrase'].slice(1)" :key="index">
           <span v-if="index"> •</span>
           <span v-for="(value, key) in data.exprs[variant]" :key="key" class="variant">{{
               data.tokens.values[data.tokens.keys.indexOf(value)].charAt(0) === '-' ? '' : ' '
-          }}{{ data.tokens.values[data.tokens.keys.indexOf(value)] }}</span>
+          }}{{ store.translit(data.tokens.values[data.tokens.keys.indexOf(value)]) }}</span>
         </span>
       </span>
       <Unit v-for="(uid, index) in value" :key="index" :uid="uid" :num="value.length > 1 ? String(index + 1) : ''"
@@ -17,19 +17,12 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import store from '../store';
 import Unit from './Unit.vue';
 
-export default {
-  name: 'UnitList',
-  setup() {
-    return { isAuth: false, data: store.state.config };
-  },
-  components: {
-    Unit,
-  },
-};
+const isAuth = false;
+const data = store.state.config;
 </script>
 
 <style>
